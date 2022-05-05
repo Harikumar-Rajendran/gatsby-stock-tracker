4b7426e9a9808d5a85b13bb7a3efa16908a39424
Harikumar <harikumar.rajendran.contractor@pepsico.com>
2022-05-05 20:17:27 +0530

    Update Package.json
    Update Package.json

--- netlify/functions/user-details.js
+++ netlify/functions/user-details.js
@@ -1,29 +1,44 @@
+var faunadb = require('faunadb'),
+  q = faunadb.query
+
 const data = {
-    "user": [
-      {
-        "FirstName": "John",
-        "LastName": "Doe",
-        "Email": "John.Doe@gmail.com",
-        "Role": "Analyst",        
-      },
-      {
-        "FirstName": "Harikumar",
-        "LastName": "Rajendran",
-        "Email": "r.hkumars@gmail.com",
-        "Role": "Admin",        
-      },
-      {
-        "FirstName": "Harikumar",
-        "LastName": "Rajendran",
-        "Email": "harikumar.rajendran@cognizant.com",
-        "Role": "Analyst",        
-      },
-    ]}
+  "user": [
+    {
+      "FirstName": "John",
+      "LastName": "Doe",
+      "Email": "John.Doe@gmail.com",
+      "Role": "Analyst",
+    },
+    {
+      "FirstName": "Harikumar",
+      "LastName": "Rajendran",
+      "Email": "r.hkumars@gmail.com",
+      "Role": "Admin",
+    },
+    {
+      "FirstName": "Harikumar",
+      "LastName": "Rajendran",
+      "Email": "harikumar.rajendran@cognizant.com",
+      "Role": "Analyst",
+    },
+  ]
+}
 
-exports.handler=async function(event,context){
+exports.handler = async function (event, context) {
+  //fauna code 
+  var client = new faunadb.Client({ secret: 'fnAEl0j-irAAR6IX8lj9-7TNb7xdkczzIvAjaTsK', domain: 'db.us.fauna.com' })
+  var resp = "";
+  var createP = client.query(
+    q.Create(q.Collection('UserTable'), { data: { userName: 'tom@gmail.com', roleName: 'Analyst' } })
+  );
 
-    return{
-        statusCode:200,
-        body:JSON.stringify({messge:data})
-    };
+  createP.then(function (response) {
+    console.log(response) // Would log the ref to console.
+    resp = response;
+  })
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ messge: data })
+  };
 }
